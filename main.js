b804c9ad680cae0fc2e37953ae26991214a7db17
jwkim <phoongdeng@gmail.com>
2020-10-20 01:54:20 +0900

    타자연습 반복로직 상세작업

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
+const modal = document.querySelector('.modal');
+const result = document.querySelector('.modal .title');
+
 const typing = typingChecker();
-typing.initialize();
+
+const makeResult = (data) => {
+  result.innerText = data;
+  modal.classList.remove('hidden');
+  document.addEventListener('keydown', handleModal);
+};
+
+const handleModal = (event) => {
+  event.preventDefault();
+  if (event.code === 'Enter') {
+    modal.classList.add('hidden');
+    document.removeEventListener('keydown', handleModal);
+    typing.initialize(makeResult);
+  }
+};
+
+document.addEventListener('keydown', handleModal);
